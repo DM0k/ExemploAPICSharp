0e6f18f7b1954ac3a119b803e16eb4f964039118
DM0k <129907524+DM0k@users.noreply.github.com>
2024-02-02 22:16:10 -0300

    Inicial Commit

---
+++ ExemploAPICSharp/ExemploAPICSharp/Controllers/PessoaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ExemploAPICSharp.Model;
+
+namespace ExemploAPICSharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+
+        [HttpGet("GetPessoa")]
+
+        public Pessoa GetPessoa() {
+            
+            var vPessoa = new Pessoa();
+            vPessoa.Id = 1;
+            vPessoa.Nome = "Zé Robson";
+            vPessoa.NumeroCelular = "(18) 990123-0123";
+
+            return vPessoa;
+            
+        }
+    }
+}
